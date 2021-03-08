004e05613ef09f4eb8e183afa18753e5993cc875
Shihanur Rahman Chowdhury <hackedgdown@gmail.com>
2021-03-08 22:16:40 +0600

    Using Fetch API, Axios API in ajax tutorial

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,51 @@
 import '../styles/index.scss';
+import axios from 'axios';
 
-import { x } from './test';
+const URL = 'https://jsonplaceholder.typicode.com/users';
 
-const fun = () => console.log("Alhamdulillah for everythings. Allahu Akbar. Start with ajax");
+let loadData = document.querySelector('#btnID');
+let output = document.querySelector('#output');
 
-fun();
+// _ _ _ _USING XMLHttpRequest_ _ _ _ _ _ _  
+// loadData.addEventListener('click', () => {
+    
+//     const xhr = new XMLHttpRequest();
 
-console.log(x);
+//     xhr.onreadystatechange = () => {
+//         output.innerHTML =  xhr.response;
+//     }
+//     xhr.open('GET', URL);
+//     xhr.send();
+
+// })
+
+// _ _ _ _USING Fetch API_ _ _ _ _ _ _
+
+// loadData.addEventListener('click', () => {
+//     fetch(URL)
+//     .then((res) => res.json())
+//     .then((res_data) => {
+//         res_data.forEach(element => {
+//             output.innerHTML += ` Name: ${element.name}<br>`; 
+//             // OR
+//             // output.innerHTML = `${output.innerHTML} Name: ${element.name} <br>`; 
+//         });
+//     })
+//     .catch((err) => console.log(err))
+// });
+
+// _ _ _ _SAME THING DONE BY USING Axios API_ _ _ _ _ _ _
+
+loadData.addEventListener('click', () => {
+    axios.get(URL)
+        .then((response) => {
+            response.data.forEach(element => {
+                output.innerHTML += ` Name: ${element.name}<br>`; 
+                // OR
+                // output.innerHTML = `${output.innerHTML} Name: ${element.name} <br>`; 
+            });
+        })
+        .catch((err) => console.log(err))
+});
+
+
